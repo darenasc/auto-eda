e7a88cf659108be38f9c0213080929585553d4c8
Diego Arenas <darenasc@gmail.com>
2019-09-04 11:45:17 +0100

    create metadata database mysql updated

--- src/explorer-meetup.py
+++ src/explorer-meetup.py
@@ -5,10 +5,6 @@ from string_connections.connections import DB_META_CONFIG, DB_EMPLOYEE_CONFIG
 SOURCE_ENGINE = 'mysql' # or one of the above
 METADATA_ENGINE = 'mysql' # or one of the above
 
-# Edit with your connections
-source_connection_params = 'string_connections/connections.py'
-metadata_connection_params = 'string_connections/connections.py'
-
 aeda.setSourceConnection(SOURCE_ENGINE, DB_EMPLOYEE_CONFIG)
 aeda.setMetadataConnection(METADATA_ENGINE, DB_META_CONFIG)
 
